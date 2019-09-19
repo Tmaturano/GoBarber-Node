376d6ba2cc6ca2384ed260d64b2c69368dfe60a7
Thiago Maturana <thiagomatu@gmail.com>
2019-09-19 09:35:09 -0300

    added JWT authentication

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+
+import authConfig from '../../config/auth';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    // https://www.md5online.org/
+    // The second parameter is a unique key to help protect the token, in MD5 format
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
